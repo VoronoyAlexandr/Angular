18aeb260ee70ecf79094a5c2d5822a1fa94bee8c
Voronoy Alexandr <servermed@gmail.com>
2015-09-03 14:29:22 +0300

    New controller, tab, images, method

--- app.js
+++ app.js
@@ -7,18 +7,42 @@
         this.products = gems
 
     });
+    app.controller('PanelController', function(){
+
+        this.tab = 1;
+        this.selectTab = function(setTab){
+            this.tab = setTab;
+        };
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        }
+
+    });
     var gems = [
+
         {
-            name: 'Dodecahedron',
-            price: 2.95,
-            description: 'Best product for best price!',
-            canPurchase: true
+            name: 'Azurite',
+            description: "Some gems have hidden qualities beyond " +
+            "their luster, beyond their shine... Azurite is one" +
+            " of those gems.",
+            price: 110.50,
+            images: [
+                "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-02.gif",
+                "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-05.gif",
+                "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-09.gif"
+            ]
         },
         {
-            name: 'Pentagonal Gem',
-            price: 5.95,
-            description: 'Best product for best price!',
-            canPurchase: true
+            name: 'Bloodstone',
+            description: "Origin of the Bloodstone is unknown, " +
+            "hence its low value. It has a very high shine and" +
+            " 12 sides, however.",
+            price: 22.90,
+            images: [
+                "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-01.gif",
+                "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-03.gif",
+                "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-04.gif",
+            ],
         }
     ]
 
